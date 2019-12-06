e0afacee687c3a1313ef1a0f0b619e327cd96d69
Albert Suqiasyan <53874213+Anteroxc@users.noreply.github.com>
2019-12-06 21:45:51 +0400

    Update MainActivity.kt

--- src/main/java/com/example/internshiptest/MainActivity.kt
+++ src/main/java/com/example/internshiptest/MainActivity.kt
@@ -9,8 +9,8 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var diceImage: ImageView
-    lateinit var diceImage1: ImageView
+    lateinit var diceImageView1: ImageView
+    lateinit var diceImageView2: ImageView
     lateinit var diceImageView3: ImageView
     lateinit var diceImageView4: ImageView
     lateinit var playerScoreTextView1: TextView
@@ -20,28 +20,28 @@ class MainActivity : AppCompatActivity() {
     var playerScore2: Int = 0
     var gameLimit = 30
     var currentPlayer = true
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        diceImage = findViewById(R.id.diceImageView)
-        diceImage1 = findViewById(R.id.diceImageView1)
-        playerScoreTextView1 = findViewById(R.id.playerTextView1)
-        playerScoreTextView2 = findViewById(R.id.playerTextView2)
+        diceImageView1 = findViewById(R.id.diceImageView)
+        diceImageView2 = findViewById(R.id.diceImageView1)
         diceImageView3 = findViewById(R.id.diceImageView3)
         diceImageView4 = findViewById(R.id.diceImageView4)
+        playerScoreTextView1 = findViewById(R.id.playerTextView1)
+        playerScoreTextView2 = findViewById(R.id.playerTextView2)
         winnerTextView = findViewById(R.id.winnerTextView)
     }
 
     private fun rollDices(): Pair<Int, Int> {
         var random: Int = Random.nextInt(6) + 1
         return when (random) {
-            1 -> Pair(R.drawable.dice_1,random)
-            2 -> Pair(R.drawable.dice_2,random)
-            3 -> Pair(R.drawable.dice_3,random)
-            4 -> Pair(R.drawable.dice_4,random)
-            5 -> Pair(R.drawable.dice_5,random)
-            else -> Pair(R.drawable.dice_6,random)
+            1 -> Pair(R.drawable.dice_1, random)
+            2 -> Pair(R.drawable.dice_2, random)
+            3 -> Pair(R.drawable.dice_3, random)
+            4 -> Pair(R.drawable.dice_4, random)
+            5 -> Pair(R.drawable.dice_5, random)
+            else -> Pair(R.drawable.dice_6, random)
         }
     }
 
@@ -50,21 +50,25 @@ class MainActivity : AppCompatActivity() {
             if (currentPlayer) {
                 var (dice1, number1) = rollDices()
                 addScore1(number1)
-                diceImage.setImageResource(dice1)
+                diceImageView1.setImageResource(dice1)
+                annimateDice(diceImageView1)
                 var (dice2, number2) = rollDices()
                 addScore1(number2)
-                diceImage1.setImageResource(dice2)
+                diceImageView2.setImageResource(dice2)
+                annimateDice(diceImageView2)
                 currentPlayer = false
             } else {
                 var (dice1, number1) = rollDices()
                 addScore2(number1)
+                annimateDice(diceImageView3)
                 diceImageView3.setImageResource(dice1)
                 var (dice2, number2) = rollDices()
                 addScore2(number2)
+                annimateDice(diceImageView4)
                 diceImageView4.setImageResource(dice2)
                 currentPlayer = true
             }
-        }else {
+        } else {
             if (playerScore1 > playerScore2) {
                 winnerTextView.text = "Player 1 Won"
             } else {
@@ -74,8 +78,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun clear(view: View) {
-        diceImage.setImageResource(R.drawable.empty_dice)
-        diceImage1.setImageResource(R.drawable.empty_dice)
+        diceImageView1.setImageResource(R.drawable.empty_dice)
+        diceImageView2.setImageResource(R.drawable.empty_dice)
         diceImageView3.setImageResource(R.drawable.empty_dice)
         diceImageView4.setImageResource(R.drawable.empty_dice)
         playerScore1 = 0
@@ -83,6 +87,7 @@ class MainActivity : AppCompatActivity() {
         playerScoreTextView1.text = "Player 1 Score: $playerScore1"
         playerScoreTextView2.text = "Player 2 Score: $playerScore2"
         winnerTextView.text = ""
+        currentPlayer = true
     }
 
     fun addScore1(diceNumber: Int) {
@@ -95,4 +100,7 @@ class MainActivity : AppCompatActivity() {
         playerScoreTextView2.text = "Player 1 Score: $playerScore2"
     }
 
+    fun annimateDice(dice: ImageView) {
+        dice.animate().rotationX(720f).duration = 1000
+    }
 }
